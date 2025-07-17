7ea1cf31a5eb68cda8e5e5638ef520e5510917ac
Milan Soucek <milan.soucek.2004@gmail.com>
2025-07-17 13:11:39 +0200

    added providers and styles folder

---
+++ my-app/src/providers/userProvider.tsx
@@ -0,0 +1,3 @@
+import React, {createContext, useEffect, useState} from 'react';
+import {useRouter} from 'next/router';
+
---
+++ my-app/src/styles/styles.ts
@@ -0,0 +1,9 @@
+export const styles = {
+    primary: "",
+    secondary: "",
+    "text-primary": " ",
+    "text-secondary": " ",
+    border: "rgba(0,0,0,0.1)",
+    error: " ",
+    success: " "
+}
