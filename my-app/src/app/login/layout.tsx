bdb8d98959a30ffa234ce7339b26454fe90feeda
Milan Soucek <milan.soucek.2004@gmail.com>
2025-07-18 15:35:23 +0200

    Layout structure changes

--- my-app/src/app/login/layout.tsx
+++ my-app/src/app/login/layout.tsx
@@ -1,34 +1,20 @@
+import React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "Login",
+  title: "AboutMe | Login",
   description: "",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const Layout = ({ children }: {children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <div
+          className={"min-h-screen bg-gradient-to-tl from-grey-100 to-blue-200  flex items-center justify-center p-4"}
       >
         {children}
-      </body>
-    </html>
+      </div>
   );
 }
+
+export default Layout;
