b3aedb3150b6d289c0ea3aabddaeaa0950682685
Milan Soucek <milan.soucek.2004@gmail.com>
2025-07-18 15:40:52 +0200

    fr idk

--- my-app/src/components/NavBar/navbarMiddle.tsx
+++ my-app/src/components/NavBar/navbarMiddle.tsx
@@ -28,7 +28,7 @@ export default function NavbarMiddle() {
     return (
         <div className="flex basis-3/4">
             <nav onClick={() => setMenuOpen(prev => !prev)} className="transition-all duration-300 ease-in-out w-1/3 lg:w-0 flex items-center justify-center touch-manipulation">
-                <img className="w-6 h-6" src="/Components/NavBar/menu.png" />
+                <Image width={15} height={15}  className="w-6 h-6" src="/Components/NavBar/menu.png" alt="Menu" />
             </nav>
             <div
                 className={`
@@ -42,12 +42,12 @@ export default function NavbarMiddle() {
                 <a href="#" className="block px-4 py-3 bg-white text-black transition-colors duration-300 active:bg-gray-200 rounded">Creator</a>
             </div>
             <nav className="w-2/3 rounded-r-xl flex items-center justify-center overflow-hidden">
-                <img src="/Components/NavBar/Logo_Brno_BLACK_RGB.png"/>
+                <Image width={260} height={60} src="/Components/NavBar/Logo_Brno_BLACK_RGB.png" alt="Logo_Brno_BLACK_RGB" />
             </nav>
-            <nav className="flex flex-row transition-all duration-2000 ease-in-in w-0 lg:w-full overflow-y-hidden">
-                <div className="flex whitespace-nowrap px-4 w-1/3 justify-center items-center hover:bg-gray-100 z-3 overflow-hidden"><a href="#" >Home</a></div>
-                <div className="flex whitespace-nowrap px-4 w-1/3 justify-center items-center hover:bg-gray-100 z-2 overflow-hidden"><a href="#" >Create About me</a></div>
-                <div className="flex whitespace-nowrap px-4 w-1/3 justify-center items-center hover:bg-gray-100 z-1 overflow-hidden"><a href="#" >Creator</a></div>
+            <nav className="flex flex-row transition-all duration-500 ease-in-in w-0 lg:w-full overflow-y-hidden">
+                <div className="flex whitespace-nowrap px-4 w-1/3 justify-center items-center hover:bg-gray-200 transition-colors duration-500 overflow-hidden"><a href="#" >Home</a></div>
+                <div className="flex whitespace-nowrap px-4 w-1/3 justify-center items-center hover:bg-gray-200 transition-colors duration-500 overflow-hidden"><a href="#" >Create About me</a></div>
+                <div className="flex whitespace-nowrap px-4 w-1/3 justify-center items-center hover:bg-gray-200 transition-colors duration-500 overflow-hidden"><a href="#" >Creator</a></div>
             </nav>
 
         </div>
