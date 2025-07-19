396ee1533e782956cdfadb8371aecc5a97acd865
Milan Soucek <milan.soucek.2004@gmail.com>
2025-07-19 20:58:28 +0200

    removing empty unused component

--- my-app/src/components/NavBar/NavList.tsx
+++
@@ -1,7 +0,0 @@
-export default function NavList() {
-    return (
-        <div className="flex-row w-full">
-
-        </div>
-    )
-}
--- my-app/src/components/NavBar/navbarLeft.tsx
+++
@@ -1,7 +0,0 @@
-export default function NavbarLeft() {
-    return (
-        <div className="basis-1/4 rounded-r-xl bg-indigo-700 ">
-
-        </div>
-    )
-}
