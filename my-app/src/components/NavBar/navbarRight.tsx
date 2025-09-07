fdeb028b7e2562b7f77c3752580089a2c0a23481
Milan Soucek <milan.soucek.2004@gmail.com>
2025-09-07 12:25:37 +0200

    adding links to buttons

--- my-app/src/components/NavBar/navbarRight.tsx
+++ my-app/src/components/NavBar/navbarRight.tsx
@@ -1,4 +1,4 @@
-
+import Link from 'next/link'
 export default function NavbarRight() {
 
 
@@ -6,11 +6,11 @@ export default function NavbarRight() {
     return (
         <div className="flex basis-1/4  items-center justify-center">
             <div className="hidden xl:block space-x-5  transition-transform duration-1000">
-                <button className="border-2 border-gray-300 transition-colors duration-300 rounded-2xl px-5 py-2 hover:bg-gray-200 hover:border-black">Sign in</button>
-                <button className="border-2 border-gray-300 transition-colors duration-300 rounded-2xl px-5 py-2 hover:bg-gray-200 hover:border-black"> Sign up</button>
+                <Link href="/login?form=signin"><button className="border-2 border-gray-300 transition-colors duration-300 rounded-2xl px-5 py-2 hover:bg-gray-200 hover:border-black">Sign in</button></Link>
+                <Link href="/login?form=signup"><button className="border-2 border-gray-300 transition-colors duration-300 rounded-2xl px-5 py-2 hover:bg-gray-200 hover:border-black"> Sign up</button></Link>
             </div>
             <div className="xl:hidden flex">
-                <img src="/Components/NavBar/user.png" alt="user" className="w-6 h-6"/>
+                <Link href="/login"><img src="/Components/NavBar/user.png" alt="user" className="w-6 h-6"/></Link>
                 {/* Add more icons here */}
             </div>
         </div>
